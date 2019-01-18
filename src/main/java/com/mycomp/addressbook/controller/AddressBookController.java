ac45a96da60c0a4030c746892ac559385d3f0e9a
vijayalakshmiputrevu <vijayalakshmiputrevu@yahoo.co.in>
2019-01-17 21:41:52 -0600

    Added a new Method

--- src/main/java/com/mycomp/addressbook/controller/AddressBookController.java
+++ src/main/java/com/mycomp/addressbook/controller/AddressBookController.java
@@ -1,8 +1,12 @@
 package com.mycomp.addressbook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AddressBookController {
-
+	@RequestMapping("welcome.htm")
+	public String welcome() {
+		return "welcome";
+	}
 }
