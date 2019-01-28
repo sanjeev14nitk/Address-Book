d08b0c457f38220f69cd6c06627044fabc0ac321
Sanjeev Kumar <sanjeev14nitk@users.noreply.github.com>
2019-01-28 15:44:46 -0600

    Changed from @service to @repository

--- src/main/java/com/mycomp/addressbook/dao/AbOperationsImpl.java
+++ src/main/java/com/mycomp/addressbook/dao/AbOperationsImpl.java
@@ -3,10 +3,13 @@ package com.mycomp.addressbook.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mycomp.addressbook.dto.Login;
 
-@Service
+@Repository
+@Transactional
 public class AbOperationsImpl implements AbOperations {
 
 	@Autowired
